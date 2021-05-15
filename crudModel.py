be94a75f9f9918561b3df6d8732975c94eec9a78
abelaba <se.abel.abate@gmail.com>
2021-05-15 19:40:30 +0300

    getfunds method added

--- crudModel.py
+++ crudModel.py
@@ -28,3 +28,42 @@ def newFund(userId,title,category,description,goal):
         if conn is not None:
             cur.close()
             conn.close()
+
+def getFunds():
+    conn = None
+    query = "select userid,campaign.id,title,category,description,campaign.balance,goal,approved,email from public.campaign JOIN public.users on campaign.userid=users.id"
+
+    try:
+        conn = psycopg2.connect("dbname=" + DBNAME + " user=" + DBUSER +" password=" +DBPASSWORD)
+        cur = conn.cursor()
+        cur.execute(query)
+        conn.commit()
+        result = cur.fetchall()
+        amount = len(result)
+        response = []
+        for r in result:
+            
+            response.append({
+                    "userid":r[0],
+                    "id":r[1],
+                    "title":r[2],
+                    "category":r[3],
+                    "description":r[4],
+                    "balance":r[5],
+                    "goal":r[6],
+                    "approved":r[7],
+                    "email":r[8]
+
+                })
+        
+        return json.dumps(response)
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+        if conn is not None:
+            cur.close()
+            conn.close()
+            return False
+    finally:
+        if conn is not None:
+            cur.close()
+            conn.close()
